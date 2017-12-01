48f896f29244f353a707bfd46e3f8f76614828d2
alpermuhacir <alpermuhacir@outlook.com>
2017-12-02 01:19:01 +0200

    CSGO CFG Creator
    Only runs x64
    An alternative application that provides cfg creation for CSGO

---
+++ WriteCFG_for_csGO/WriteCFG_for_csGO/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace WriteCFG_for_csGO
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCfgYaz = new System.Windows.Forms.TextBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtOku = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Elephant", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(504, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Write New CFG";
+            // 
+            // txtCfgYaz
+            // 
+            this.txtCfgYaz.BackColor = System.Drawing.Color.Black;
+            this.txtCfgYaz.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtCfgYaz.Cursor = System.Windows.Forms.Cursors.Default;
+            this.txtCfgYaz.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtCfgYaz.ForeColor = System.Drawing.SystemColors.Info;
+            this.txtCfgYaz.Location = new System.Drawing.Point(508, 70);
+            this.txtCfgYaz.Multiline = true;
+            this.txtCfgYaz.Name = "txtCfgYaz";
+            this.txtCfgYaz.Size = new System.Drawing.Size(375, 415);
+            this.txtCfgYaz.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Desktop;
+            this.pictureBox1.BackgroundImage = global::WriteCFG_for_csGO.Properties.Resources.csgo_logo;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.pictureBox1.Enabled = false;
+            this.pictureBox1.Location = new System.Drawing.Point(752, 520);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(174, 49);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.UseWaitCursor = true;
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.BackColor = System.Drawing.Color.Transparent;
+            this.btnKaydet.BackgroundImage = global::WriteCFG_for_csGO.Properties.Resources.Floppy_5_25_inch_icon;
+            this.btnKaydet.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnKaydet.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnKaydet.Location = new System.Drawing.Point(508, 508);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(64, 61);
+            this.btnKaydet.TabIndex = 1;
+            this.btnKaydet.UseVisualStyleBackColor = false;
+            this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Elephant", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(516, 491);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 14);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "SAVE";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Elephant", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(359, 491);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 14);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Open File";
+            // 
+            // txtOku
+            // 
+            this.txtOku.BackColor = System.Drawing.Color.Black;
+            this.txtOku.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtOku.Enabled = false;
+            this.txtOku.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtOku.ForeColor = System.Drawing.SystemColors.Info;
+            this.txtOku.Location = new System.Drawing.Point(46, 70);
+            this.txtOku.Multiline = true;
+            this.txtOku.Name = "txtOku";
+            this.txtOku.Size = new System.Drawing.Size(375, 415);
+            this.txtOku.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.BackgroundImage = global::WriteCFG_for_csGO.Properties.Resources.White_Folder_Documents_icon;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Location = new System.Drawing.Point(357, 508);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(64, 61);
+            this.button1.TabIndex = 6;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Elephant", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(42, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 21);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Read CFG";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Enabled = false;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(3, 563);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(104, 9);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Copyright AlpMHCR ©  2017";
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.BackgroundImage = global::WriteCFG_for_csGO.Properties.Resources.who_owns_the_digital_map_of_the_world_laura_bliss_citylab;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(928, 572);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtOku);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtCfgYaz);
+            this.Controls.Add(this.btnKaydet);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Opacity = 0.95D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CFG";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnKaydet;
+        private System.Windows.Forms.TextBox txtCfgYaz;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtOku;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+    }
+}
+
---
+++ WriteCFG_for_csGO/WriteCFG_for_csGO/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace WriteCFG_for_csGO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                saveFileDialog1.FileName = "*";
+                saveFileDialog1.Filter = "Configure Files (*cfg)|*.cfg";
+                saveFileDialog1.DefaultExt = "cfg";
+                saveFileDialog1.ShowDialog();
+
+                StreamWriter yazmaIslemi = new StreamWriter(saveFileDialog1.FileName);
+                yazmaIslemi.WriteLine(txtCfgYaz.Text);
+                yazmaIslemi.Close();
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+                throw;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog ofd = new OpenFileDialog();
+                ofd.ShowDialog();
+                StreamReader okumaIslemi = new StreamReader(ofd.FileName);
+                txtOku.Text = okumaIslemi.ReadToEnd();
+                okumaIslemi.Close();
+                /*openFileDialog2.FileName = "Okumak İstediğiniz CFG Dosyasını Seçiniz.";
+                openFileDialog2.Filter = "Configure Files (*cfg)|*.cfg";
+                openFileDialog2.OpenFile();
+
+                StreamReader okumaIslemi = new StreamReader(openFileDialog2.FileName);
+                txtOku.Text = okumaIslemi.ReadToEnd();
+                okumaIslemi.Close();*/
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+                throw;
+            }
+        }
+    }
+}
